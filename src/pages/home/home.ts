3512b0de343def68990a23cb523d61cd99fe1af8
Cleber Santos <oclebersantos@gmail.com>
2017-12-29 16:19:30 -0200

    conectando no back

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsuarioProvider } from '../../providers/usuario/usuario';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [UsuarioProvider]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public usuarios = [];
 
+  constructor(public navCtrl: NavController, private usuarioService:UsuarioProvider) {
+
+    this.getUsuarios();
+
+  }
+
+  public getUsuarios() {
+  	this.usuarioService.findAll().subscribe(response => this.usuarios = response);
   }
 
 }
